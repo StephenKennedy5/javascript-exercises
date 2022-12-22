cd6913cbf50d2ae6db52685ff8ef308df8dde02a
StephenKennedy5 <5nehpets5kennedy@gmail.com>
2022-12-22 12:01:58 -0800

    Answers to 02 problems

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let new_string = ''
+    if (num < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < num; i++) {
+        new_string += string
+    } 
+    return new_string;
 };
 
 // Do not edit below this line

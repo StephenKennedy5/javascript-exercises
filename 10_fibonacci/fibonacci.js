880531098bd24728bdf46d3652824bf286abfb47
StephenKennedy5 <5nehpets5kennedy@gmail.com>
2022-12-22 15:37:59 -0800

    answer to 10 problems

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    
+    var a = 1, b = 0, temp;
+
+    if (num < 0) {
+        return 'OOPS'
+    }
+
+    while (num > 0) {
+        temp = a;
+        a = a + b;
+        b = temp;
+        num--;
+    }
+
+    return b
 
 };
 

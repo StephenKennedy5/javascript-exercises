d40b3110e2946791ada61b768ecd7b76d0269cb9
StephenKennedy5 <5nehpets5kennedy@gmail.com>
2022-12-22 13:52:20 -0800

    Answers to 05 problem

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    let sumval = 0
 
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    } else if (num1.length > 1 || num2.length > 1) {
+        return "ERROR";
+    } else if (num1.type === "string" || num2.type === "string") {
+        return "ERROR";
+    }
+
+
+
+    if (num1 > num2) {
+        for (let i = num2; i <= num1; i++) {
+            sumval += i;
+        }
+    } else {
+        for (let i = num1; i <= num2; i++) {
+            sumval += i;
+        }
+    }
+    return sumval
 };
 
 // Do not edit below this line

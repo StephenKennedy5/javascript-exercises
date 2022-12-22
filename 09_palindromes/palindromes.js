61c4a0a3928cfc1096188b7176cd90b4c1007737
StephenKennedy5 <5nehpets5kennedy@gmail.com>
2022-12-22 15:23:22 -0800

    Answers to 09 problems

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let newstring = string.replace(/\W/g, '').toLowerCase();
+    let reversestring = '';
+    for (let i = newstring.length-1; i >= 0; i--) {
+        reversestring += newstring[i]
+    }
 
+    return newstring === reversestring;
 };
 
 // Do not edit below this line

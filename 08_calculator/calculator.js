28667015be71f6e36927c3c07baf8bb6afe81071
StephenKennedy5 <5nehpets5kennedy@gmail.com>
2022-12-22 14:59:51 -0800

    Answers to 08 problems

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sumarray = 0;
+  for (let i = 0; i < array.length; i++) {
+    sumarray += array[i];
+  }
+  return sumarray;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multiplyarray = 1;
+  for (let i = 0; i < array.length; i++) {
+    multiplyarray *= array[i]
+  }
+  return multiplyarray;
 };
 
-const power = function() {
-	
+const power = function(num,exp) {
+	return num**exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let fact = 1;
+  for (let i = 1; i <= num; i++) {
+    fact *= i
+  }
+  return fact;
 };
 
 // Do not edit below this line
